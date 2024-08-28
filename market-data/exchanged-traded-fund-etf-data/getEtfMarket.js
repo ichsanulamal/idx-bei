bee6609fb4a8ef89142a01cab0dc4da7aea86565
ichsanulamal <muhammad.ichsanul19@gmail.com>
2024-08-28 19:34:45 +0700

    fix etf

--- market-data/exchanged-traded-fund-etf-data/getEtfMarket.js
+++ market-data/exchanged-traded-fund-etf-data/getEtfMarket.js
@@ -1,14 +1,23 @@
 import { fetchData } from "../../utils/template.js";
 
-export async function getEtfMarket() {
-  const url = "https://www.idx.co.id/primary/EDD/GetEtfMarket?length=9999";
+/**
+ * Retrieves ETF market data from IDX.
+ * @param {number} [length=9999] - The number of records to retrieve.
+ * @returns {Promise<string>} - A JSON string of the ETF market data.
+ */
+export async function getEtfMarket({
+  length = 9999
+} = {}) {
+  const baseUrl = "https://www.idx.co.id/primary/EDD/GetEtfMarket";
+  const queryParams = new URLSearchParams({ length }).toString();
+  const url = `${baseUrl}?${queryParams}`;
   const referrer = "https://www.idx.co.id/en/market-data/exchanged-traded-fund-etf-data/";
 
   try {
-    const res = await fetchData(url, referrer);
-    return JSON.stringify(res, null, 2);
+    const response = await fetchData(url, referrer);
+    return JSON.stringify(response, null, 2);
   } catch (error) {
-    console.error("Error fetching boards:", error);
-    throw error;
+    console.error("Error fetching ETF market data:", error.message);
+    throw new Error(`Failed to fetch ETF market data: ${error.message}`);
   }
 }
